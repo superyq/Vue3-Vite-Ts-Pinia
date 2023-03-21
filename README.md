df3ba75a9e933b551fea738914a2f6f96850d7bf
yqcoder <447845126@qq.com>
2023-03-21 15:46:01 +0800

    chore: 构建项目

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vue 3 + TypeScript + Vite
+
+## src/vite-env.d.ts
+作用：1.声明环境变量内容。2.支持在ts文件中识别.vue文件
---
+++ src/pages/login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="login">
+    login
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
